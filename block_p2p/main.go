d60a940d7c5b0fe8d4594d0e77972f9220927370
xuyp1991 <xuyapeng1991@qq.com>
2020-08-28 14:12:58 +0800

    some code commit

--- block_p2p/main.go
+++ block_p2p/main.go
@@ -12,9 +12,9 @@ import (
     "io"
     "log"
     mrand "math/rand"
-     "os"
-    // "strconv"
-     "strings"
+    "os"
+    "strconv"
+    "strings"
     "sync"
     "time"
 
@@ -30,50 +30,69 @@ import (
 	gologv2 "github.com/ipfs/go-log/v2"
 )
 
-
-
-var (
-    mutex = &sync.Mutex{}
-    Owner string
-
-    Datachan = make(chan []byte)
-)
-
-
-var  msgStore map[string]MessageInfo 
-// //构造一个结构体,存储消息,校验消息有没有接收过
-type MessageInfo struct {
-	Timestamp string
+// Block represents each 'item' in the blockchain
+type Block struct {
+    Index     int
+    Timestamp string
+    BPM       int
     Hash      string
-    From      string
-	To string
-	Data []byte
+    PrevHash  string
 }
 
-func calculateHash(msg MessageInfo) string {
-    record := msg.Timestamp + msg.From + msg.To + string(msg.Data)
+// Blockchain is a series of validated Blocks
+var Blockchain []Block
+
+var mutex = &sync.Mutex{}
+
+// make sure block is valid by checking index, and comparing the hash of the previous block
+func isBlockValid(newBlock, oldBlock Block) bool {
+    if oldBlock.Index+1 != newBlock.Index {
+        return false
+    }
+
+    if oldBlock.Hash != newBlock.PrevHash {
+        return false
+    }
+
+    if calculateHash(newBlock) != newBlock.Hash {
+        return false
+    }
+
+    return true
+}
+
+// SHA256 hashing
+func calculateHash(block Block) string {
+    record := strconv.Itoa(block.Index) + block.Timestamp + strconv.Itoa(block.BPM) + block.PrevHash
     h := sha256.New()
     h.Write([]byte(record))
     hashed := h.Sum(nil)
     return hex.EncodeToString(hashed)
 }
 
-func NewMsgInfo(from,to string,data []byte) MessageInfo {
-    var result MessageInfo
+// create a new block using previous block's hash
+func generateBlock(oldBlock Block, BPM int) Block {
+
+    var newBlock Block
+
     t := time.Now()
 
-    result.From = from
-    result.To = to
-    result.Data = data
-    result.Timestamp = t.String()
-    result.Hash = calculateHash(result)
-    return result
+    newBlock.Index = oldBlock.Index + 1
+    newBlock.Timestamp = t.String()
+    newBlock.BPM = BPM
+    newBlock.PrevHash = oldBlock.Hash
+    newBlock.Hash = calculateHash(newBlock)
+
+    return newBlock
 }
 
 // makeBasicHost creates a LibP2P host with a random peer ID listening on the
 // given multiaddress. It will use secio if secio is true.
 func makeBasicHost(listenPort int, secio bool, randseed int64) (host.Host, error) {
 
+    // If the seed is zero, use real cryptographic randomness. Otherwise, use a
+    // deterministic randomness source to make generated keys stay the same
+    // across multiple runs
     var r io.Reader
     if randseed == 0 {
         r = rand.Reader
@@ -119,7 +138,6 @@ func makeBasicHost(listenPort int, secio bool, randseed int64) (host.Host, error
     return basicHost, nil
 }
 
-
 func handleStream(s net.Stream) {
 
     log.Println("Got a new stream!")
@@ -133,7 +151,6 @@ func handleStream(s net.Stream) {
     // stream 's' will stay open until you close it (or the other side closes it).
 }
 
-
 func readData(rw *bufio.ReadWriter) {
 
     for {
@@ -146,54 +163,49 @@ func readData(rw *bufio.ReadWriter) {
             return
         }
         if str != "\n" {
-            
-            var msgInfo MessageInfo
-            if err := json.Unmarshal([]byte(str), &msgInfo); err != nil {
+
+            chain := make([]Block, 0)
+            if err := json.Unmarshal([]byte(str), &chain); err != nil {
                 log.Fatal(err)
             }
 
-           if _, ok := msgStore["1"]; ok {
-                continue
+            mutex.Lock()
+            if len(chain) > len(Blockchain) {
+                Blockchain = chain
+                bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+                if err != nil {
+
+                    log.Fatal(err)
+                }
+                // Green console color:     \x1b[32m
+                // Reset console color:     \x1b[0m
+                fmt.Printf("\x1b[32m%s\x1b[0m> ", string(bytes))
             }
+            mutex.Unlock()
+        }
+    }
+}
+
+func writeData(rw *bufio.ReadWriter) {
+
+    go func() {
+        for {
+            time.Sleep(5 * time.Second)
+            mutex.Lock()
+            bytes, err := json.Marshal(Blockchain)
+            if err != nil {
+                log.Println(err)
+            }
+            mutex.Unlock()
 
             mutex.Lock()
-            msgStore[msgInfo.Hash] =  msgInfo
-            fmt.Printf("\x1b[32m%s\x1b[0m> ",str)
+            rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+            rw.Flush()
             mutex.Unlock()
 
         }
-    }
-}
+    }()
 
-//发送什么样子的消息?
-func writeData(rw *bufio.ReadWriter) {
-    go getStdInData()
-
-    for {
-		select {
-            case sendData := <-Datachan:
-                newMsg := NewMsgInfo(Owner,"all",[]byte(sendData))
-                mutex.Lock()
-                msgStore[newMsg.Hash] =  newMsg
-                mutex.Unlock()
-
-                bytes, err := json.Marshal(newMsg)
-                if err != nil {
-                    log.Println(err)
-                }
-
-                spew.Dump(sendData)
-
-                mutex.Lock()
-                rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
-                rw.Flush()
-                mutex.Unlock()
-        }
-    }
-
-}
-
-func getStdInData() {
     stdReader := bufio.NewReader(os.Stdin)
 
     for {
@@ -204,36 +216,62 @@ func getStdInData() {
         }
 
         sendData = strings.Replace(sendData, "\n", "", -1)
+        bpm, err := strconv.Atoi(sendData)
         if err != nil {
             log.Fatal(err)
         }
-        Datachan<- []byte(sendData)
+        newBlock := generateBlock(Blockchain[len(Blockchain)-1], bpm)
+
+        if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+            mutex.Lock()
+            Blockchain = append(Blockchain, newBlock)
+            mutex.Unlock()
+        }
+
+        bytes, err := json.Marshal(Blockchain)
+        if err != nil {
+            log.Println(err)
+        }
+
+        spew.Dump(Blockchain)
+
+        mutex.Lock()
+        rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+        rw.Flush()
+        mutex.Unlock()
     }
+
 }
 
 func main() {
+    t := time.Now()
+    genesisBlock := Block{}
+    genesisBlock = Block{0, t.String(), 0, calculateHash(genesisBlock), ""}
 
+    Blockchain = append(Blockchain, genesisBlock)
+
+    // LibP2P code uses golog to log messages. They log with different
+    // string IDs (i.e. "swarm"). We can control the verbosity level for
+    // all loggers with:
+	//golog.SetAllLoggers(gologging.INFO) // Change to DEBUG for extra info
 	golog.SetAllLoggers(gologv2.LevelInfo)
 
+    // Parse options from the command line
     listenF := flag.Int("l", 0, "wait for incoming connections")
-	target := flag.String("d", "", "target peer to dial")
-	owner := flag.String("o", "", "owner of the node")
+    target := flag.String("d", "", "target peer to dial")
     secio := flag.Bool("secio", false, "enable secio")
     seed := flag.Int64("seed", 0, "set random seed for id generation")
     flag.Parse()
-
-    Owner = *owner
 
     if *listenF == 0 {
         log.Fatal("Please provide a port to bind on with -l")
     }
 
+    // Make a host that listens on the given multiaddress
     ha, err := makeBasicHost(*listenF, *secio, *seed)
     if err != nil {
         log.Fatal(err)
     }
-
-    msgStore = make(map[string]MessageInfo)
 
     if *target == "" {
         log.Println("listening for connections")
@@ -288,6 +326,7 @@ func main() {
         go writeData(rw)
         go readData(rw)
 
-		select {} // hang forever
-	}
+        select {} // hang forever
+
+    }
 }
